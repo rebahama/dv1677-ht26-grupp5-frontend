a130ea33c4a8a20066e4659575f24c12a32d5029
rebahama <97362222+rebahama@users.noreply.github.com>
2026-09-25 21:50:51 +0000

    fix react router for vite and deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dv1677-ht26-grupp5-frontend">
       <App />
     </BrowserRouter>
   </StrictMode>,
